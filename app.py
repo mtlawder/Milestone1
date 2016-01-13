145a05af7468e91b87590dd388e034024c546170
mtlawder <mtlawder@gmail.com>
2016-01-13 11:21:56 -0500

    Cards

--- app.py
+++ app.py
@@ -53,11 +53,11 @@ def index_Main():
             node1s=NODE_info.loc[nodefind]['STATE']
             node1t=NODE_info.loc[nodefind]['TYPE']
             #return render_template('/Milestone_Main.html',Nodename="",node1n=node1n,node1s=node1s,node1t=node1t)
-            return redirect('Onenode_plot.html')
+            return redirect('Onenode_plot',node1n=node1n)
 
 @app.route('/Onenode_plot',methods=['GET','POST'])
 def Onenode_plot():
-    return render_template('Onenode_plot.html',node1n="where")
+    return render_template('Onenode_plot.html',node1n=node1n)
 
     
 
