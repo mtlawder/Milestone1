4589bc175f2628e06d6c48262c6aa3adb70d4e17
mtlawder <mtlawder@gmail.com>
2016-01-13 17:06:34 -0500

    Cards

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def index_Main():
         return render_template('/Milestone_Main.html', Nodename="",node1n="",node1s="",node1t="")
     else:
         node=request.form['nodename']
-        nodenum=request.form['nodenum']
+        #nodenum=request.form['nodenum']
         NODE_info=pd.read_csv('N_info.csv')
         #node1=request.args.get("node1")
         #Hval2=Hval.loc[0]['NODE_NAME']
@@ -51,22 +51,22 @@ def index_Main():
             node1n=NODE_info.loc[nodefind]['NODE_NAME']
             node1s=NODE_info.loc[nodefind]['STATE']
             node1t=NODE_info.loc[nodefind]['TYPE']
-            if nodenum=='1nodes':
-                dfprice=plotbokeh(node1n)
-                bdate=np.array(dfprice['DATE'], dtype=np.datetime64)
-                bprice=np.array(dfprice['PRICE'])
-                p1=figure(x_axis_type='datetime')
-                p1.line(bdate,bprice)
+            #if nodenum=='1nodes':
+            dfprice=plotbokeh(node1n)
+            bdate=np.array(dfprice['DATE'], dtype=np.datetime64)
+            bprice=np.array(dfprice['PRICE'])
+            p1=figure(x_axis_type='datetime')
+            p1.line(bdate,bprice)
                 #np.array(
                 #,dtype=np.datetime64)
-                p1.title = ' Energy Prices for ' + node1n
-                p1.xaxis.axis_label = "Date"
-                p1.yaxis.axis_label = "Price/MWh"
-                script, div = components(p1)
+            p1.title = ' Energy Prices for ' + node1n
+            p1.xaxis.axis_label = "Date"
+            p1.yaxis.axis_label = "Price/MWh"
+            script, div = components(p1)
                 #return render_template('/Milestone_Main.html',Nodename="",node1n=node1n,node1s=node1s,node1t=node1t)
-            else:
-                script='empty'
-                div='empty'
+            #else:
+            #    script='empty'
+            #    div='empty'
             return render_template('Onenode_plot.html',node1n=node1n, script=script, div=div)
 
 #@app.route('/Onenode_plot',methods=['GET','POST'])
